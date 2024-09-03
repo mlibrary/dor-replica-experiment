6bcf35d47fccf4a44d9f34bf222679e337bfacc4
Samuel Sciolla <ssciolla@umich.edu>
2024-09-03 09:55:17 -0400

    Remove logging, tidy up in registry

--- src/main/java/edu/umich/lib/dor/replicaexperiment/service/RepositoryClientRegistry.java
+++ src/main/java/edu/umich/lib/dor/replicaexperiment/service/RepositoryClientRegistry.java
@@ -3,15 +3,9 @@ package edu.umich.lib.dor.replicaexperiment.service;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import edu.umich.lib.dor.replicaexperiment.ReplicaExperimentApplication;
 import edu.umich.lib.dor.replicaexperiment.exception.RepositoryNotRegisteredException;
 
 public class RepositoryClientRegistry {
-	private static final Log log = LogFactory.getLog(ReplicaExperimentApplication.class);
-
     HashMap<String, RepositoryClient> clientMap = new HashMap<>();
 
     public void register(String name, RepositoryClient client) {
@@ -23,8 +17,6 @@ public class RepositoryClientRegistry {
     }
 
     public RepositoryClient getClient(String name) {
-        log.info(clientMap);
-        log.info(name);
         RepositoryClient client = clientMap.get(name);
         if (client == null) {
             throw new RepositoryNotRegisteredException(
