0df2a6d37885d6651d65eabc06c03341557d05db
Samuel Sciolla <ssciolla@umich.edu>
2024-09-04 16:19:19 -0400

    Use SLF4J instead of Apache Commons

--- src/main/java/edu/umich/lib/dor/replicaexperiment/service/RepositoryService.java
+++ src/main/java/edu/umich/lib/dor/replicaexperiment/service/RepositoryService.java
@@ -1,7 +1,7 @@
 package edu.umich.lib.dor.replicaexperiment.service;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +11,7 @@ import edu.umich.lib.dor.replicaexperiment.domain.RepositoryType;
 
 @Service
 public class RepositoryService {
-	private static final Log log = LogFactory.getLog(RepositoryService.class);
+	private static final Logger log = LoggerFactory.getLogger(RepositoryService.class);
 
     @Autowired
     RepositoryRepository repositoryRepo;
