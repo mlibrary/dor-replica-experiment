82a991cac18d8da40ce616af500d93fdcf80c96f
Samuel Sciolla <ssciolla@umich.edu>
2024-09-12 15:19:57 -0400

    Use DigestUtils.sha512Hex instead of Hex; bump commons-codec; use alternative exception signature for ChecksumCalculationException

--- src/main/java/edu/umich/lib/dor/replicaexperiment/service/ChecksumCalculator.java
+++ src/main/java/edu/umich/lib/dor/replicaexperiment/service/ChecksumCalculator.java
@@ -1,30 +1,21 @@
 package edu.umich.lib.dor.replicaexperiment.service;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
-import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import edu.umich.lib.dor.replicaexperiment.exception.ChecksumCalculationException;
 
 public class ChecksumCalculator {
     public static String calculate(Path filePath) {
-        MessageDigest messageDigest;
-        byte[] digestBytes;
-
         try {
-            messageDigest = MessageDigest.getInstance("SHA-512");
-        } catch (NoSuchAlgorithmException e) {
-            throw new ChecksumCalculationException(e.getMessage());
-        }
-        try {
-            digestBytes = messageDigest.digest(Files.readAllBytes(filePath));
+            return DigestUtils.sha512Hex(new FileInputStream(filePath.toFile()));
         } catch (IOException e) {
-            throw new ChecksumCalculationException(e.getMessage());
+            throw new ChecksumCalculationException(
+                "Could not calculate checksum for path: " + filePath, e
+            );
         }
-        return Hex.encodeHexString(digestBytes);
     }
 }
