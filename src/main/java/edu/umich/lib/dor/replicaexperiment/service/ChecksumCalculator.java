85f025b03ae7508b13c8a2999623f6bf38ab8d58
Samuel Sciolla <ssciolla@umich.edu>
2024-09-12 14:36:25 -0400

    Switch to commons-codec; move hex string generation out of try block; improve variable names

--- src/main/java/edu/umich/lib/dor/replicaexperiment/service/ChecksumCalculator.java
+++ src/main/java/edu/umich/lib/dor/replicaexperiment/service/ChecksumCalculator.java
@@ -6,24 +6,25 @@ import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import at.favre.lib.bytes.Bytes;
+import org.apache.commons.codec.binary.Hex;
 
 import edu.umich.lib.dor.replicaexperiment.exception.ChecksumCalculationException;
 
 public class ChecksumCalculator {
     public static String calculate(Path filePath) {
-        MessageDigest md;
+        MessageDigest messageDigest;
+        byte[] digestBytes;
+
         try {
-            md = MessageDigest.getInstance("SHA-512");
+            messageDigest = MessageDigest.getInstance("SHA-512");
         } catch (NoSuchAlgorithmException e) {
             throw new ChecksumCalculationException(e.getMessage());
         }
-
         try {
-            byte[] digest = md.digest(Files.readAllBytes(filePath));
-            return Bytes.wrap(digest).encodeHex();
+            digestBytes = messageDigest.digest(Files.readAllBytes(filePath));
         } catch (IOException e) {
             throw new ChecksumCalculationException(e.getMessage());
         }
+        return Hex.encodeHexString(digestBytes);
     }
 }
