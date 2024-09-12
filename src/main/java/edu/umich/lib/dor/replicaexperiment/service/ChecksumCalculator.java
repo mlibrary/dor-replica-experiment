a6cde2d7d4acf67206aaadf7b14e9e0afbd04978
Samuel Sciolla <ssciolla@umich.edu>
2024-09-12 13:21:57 -0400

    Fix ChecksumCalculator using Bytes, hexadecimal conversion; add test

--- src/main/java/edu/umich/lib/dor/replicaexperiment/service/ChecksumCalculator.java
+++ src/main/java/edu/umich/lib/dor/replicaexperiment/service/ChecksumCalculator.java
@@ -1,19 +1,29 @@
 package edu.umich.lib.dor.replicaexperiment.service;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import at.favre.lib.bytes.Bytes;
 
 import edu.umich.lib.dor.replicaexperiment.exception.ChecksumCalculationException;
 
 public class ChecksumCalculator {
     public static String calculate(Path filePath) {
+        MessageDigest md;
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new ChecksumCalculationException(e.getMessage());
+        }
+
+        try {
             byte[] digest = md.digest(Files.readAllBytes(filePath));
-            return new String(digest, "US-ASCII");
-        } catch(Exception e) {
-            throw new ChecksumCalculationException(e.toString());
+            return Bytes.wrap(digest).encodeHex();
+        } catch (IOException e) {
+            throw new ChecksumCalculationException(e.getMessage());
         }
     }
 }
